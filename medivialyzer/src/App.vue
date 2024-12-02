d606851528d81502c91e27869cc64e4155407747
alex1431999 <alexhaller99@yahoo.de>
2024-12-02 20:45:51 +0100

    refactor: move lootReader instantiation to lootReader.ts

--- medivialyzer/src/App.vue
+++ medivialyzer/src/App.vue
@@ -1,22 +1,8 @@
 <script setup lang="ts">
 
-import {LootReader} from "./utils/lootReader/lootReader.ts";
+import {lootReader} from "./utils/lootReader/lootReader";
 
-
-//const path = 'D:\\projects\\Medivialyzer/medivialyzer/src/utils/lootReader/Loot.txt'
-//const lootData = window.electron.fs.readFileSync(path).toString();
-const lootData = "\n" +
-    "Channel saved at Wed Nov 27 19:34:52 2024\n" +
-    "19:19 Loot of rotworm: ham.\n" +
-    "19:19 Loot of rotworm: ham, meat, a bag.\n" +
-    "19:19 Content of a bag within the corpse of rotworm: a sword.\n" +
-    "19:19 Loot of rotworm: meat, 10 gold coins, a gold coin.\n" +
-    "19:34 Loot of rotworm: ham, a bag.\n" +
-    "19:34 Content of a bag within the corpse of rotworm: a sword."
-
-const test = new LootReader(lootData);
-
-const loot = test.getLoot(0)
+const loot = lootReader.getLoot(0)
 
 </script>
 
--- medivialyzer/src/utils/lootReader/lootReader.ts
+++ medivialyzer/src/utils/lootReader/lootReader.ts
@@ -38,3 +38,16 @@ export class LootReader {
         this.lootData.split("\n").forEach(callback)
     }
 }
+
+//const path = 'D:\\projects\\Medivialyzer/medivialyzer/src/utils/lootReader/Loot.txt'
+//const lootData = window.electron.fs.readFileSync(path).toString();
+const lootData = "\n" +
+    "Channel saved at Wed Nov 27 19:34:52 2024\n" +
+    "19:19 Loot of rotworm: ham.\n" +
+    "19:19 Loot of rotworm: ham, meat, a bag.\n" +
+    "19:19 Content of a bag within the corpse of rotworm: a sword.\n" +
+    "19:19 Loot of rotworm: meat, 10 gold coins, a gold coin.\n" +
+    "19:34 Loot of rotworm: ham, a bag.\n" +
+    "19:34 Content of a bag within the corpse of rotworm: a sword."
+
+export const lootReader = new LootReader(lootData);
