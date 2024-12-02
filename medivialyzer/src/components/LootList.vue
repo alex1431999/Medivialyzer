e8ab78cbefe09f09428ef82dcdfd624e8274e903
alex1431999 <alexhaller99@yahoo.de>
2024-12-02 20:55:49 +0100

    feat: implement LootListItem.vue

--- medivialyzer/src/components/LootList.vue
+++ medivialyzer/src/components/LootList.vue
@@ -1,10 +1,12 @@
 <script setup lang="ts">
 import { defineProps } from 'vue'
 import { Loot } from '../utils/lootReader/lootReader.types'
+import LootListItem from './LootListItem.vue'
 
 const { loot } = defineProps<{ loot: Loot[] }>()
+
 </script>
 
 <template>
-  {{loot}}
+  <LootListItem v-for="lootEntry in loot" :loot-entry="lootEntry"></LootListItem>
 </template>
---
+++ medivialyzer/src/components/LootListItem.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+import { Loot } from '../utils/lootReader/lootReader.types'
+
+const { lootEntry } = defineProps<{ lootEntry: Loot }>()
+
+</script>
+
+<template>
+  {{ lootEntry }}
+</template>
