c4a4379866e07667cfb28a25f027e60650fefa22
alex1431999 <alexhaller99@yahoo.de>
2024-12-03 20:06:35 +0100

    feat: add total amount

--- frontend/src/components/LootListItem.vue
+++ frontend/src/components/LootListItem.vue
@@ -1,23 +1,36 @@
 <script setup lang="ts">
-import { defineProps } from 'vue'
+import {computed, defineProps} from 'vue'
 
 import { LootEntry} from "../utils/loot/loot.types.ts";
 import { VCard, VCardText } from "vuetify/components/VCard";
 import { VChip } from "vuetify/components/VChip";
 import { VBadge } from "vuetify/components/VBadge";
+import { VIcon } from "vuetify/components/VIcon";
 
 const { lootEntry } = defineProps<{ lootEntry: LootEntry }>()
+
+const totalValue = computed(() =>
+  lootEntry.item.value * lootEntry.amount
+)
 
 </script>
 
 <template>
   <VCard class="loot-list-item__card">
-    <VCardText>
-      <VBadge color="primary" :content="`${lootEntry.amount}x`">
-        <VChip>
+    <VCardText class="loot-list-item__text">
+      <!-- Item name -->
+      <v-badge color="secondary" :content="`${lootEntry.amount}x`">
+        <v-chip>
           {{ lootEntry.item.name }}
-        </VChip>
-      </VBadge>
+        </v-chip>
+      </v-badge>
+
+      <!-- Total value -->
+      <v-chip color="secondary">
+        {{ totalValue }}
+        <v-icon icon="mdi-gold">
+        </v-icon>
+      </v-chip>
     </VCardText>
   </VCard>
 </template>
@@ -25,5 +38,10 @@ const { lootEntry } = defineProps<{ lootEntry: LootEntry }>()
 <style scoped>
 .loot-list-item__card {
   width: 100%;
+}
+
+.loot-list-item__text {
+  display: flex;
+  justify-content: space-between;
 }
 </style>
