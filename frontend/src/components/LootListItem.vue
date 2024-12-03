58c1a1f4d0152f08ea96b9e09f31d4964c4c30b8
alex1431999 <alexhaller99@yahoo.de>
2024-12-03 19:55:34 +0100

    feat: style the loot list item a bit

--- frontend/src/components/LootListItem.vue
+++ frontend/src/components/LootListItem.vue
@@ -3,6 +3,8 @@ import { defineProps } from 'vue'
 
 import { LootEntry} from "../utils/loot/loot.types.ts";
 import { VCard, VCardText } from "vuetify/components/VCard";
+import { VChip } from "vuetify/components/VChip";
+import { VBadge } from "vuetify/components/VBadge";
 
 const { lootEntry } = defineProps<{ lootEntry: LootEntry }>()
 
@@ -11,8 +13,11 @@ const { lootEntry } = defineProps<{ lootEntry: LootEntry }>()
 <template>
   <VCard class="loot-list-item__card">
     <VCardText>
-      {{ lootEntry.item.name }}
-      {{ lootEntry.amount }}
+      <VBadge color="primary" :content="`${lootEntry.amount}x`">
+        <VChip>
+          {{ lootEntry.item.name }}
+        </VChip>
+      </VBadge>
     </VCardText>
   </VCard>
 </template>
