48901bcbd541add15e279234dca87b18856f785a
alex1431999 <alexhaller99@yahoo.de>
2024-12-03 20:20:38 +0100

    feat: add header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,10 +1,18 @@
 <script setup lang="ts">
 import LootList from './components/LootList.vue'
+import Header from "./components/Header.vue";
 import {lootReader} from "./utils/lootReader/lootReader";
 
 const loot = lootReader.getLoot(0)
 </script>
 
 <template>
-    <LootList :loot="loot" ></LootList>
+    <Header class="app__header" />
+    <LootList :loot="loot" />
 </template>
+
+<style scoped>
+.app__header {
+  margin-bottom: 16px;
+}
+</style>
---
+++ frontend/src/components/Header.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+import { VToolbar, VToolbarTitle } from "vuetify/components";
+
+</script>
+
+<template>
+  <v-toolbar>
+    <v-toolbar-title>Medivialyzer</v-toolbar-title>
+  </v-toolbar>
+</template>
