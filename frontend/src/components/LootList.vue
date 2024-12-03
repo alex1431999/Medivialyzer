b7ae5a4f692c1f805d498297d358abe436bf36d4
alex1431999 <alexhaller99@yahoo.de>
2024-12-03 20:36:56 +0100

    feat: add loot list menu

--- frontend/src/components/LootList.vue
+++ frontend/src/components/LootList.vue
@@ -1,19 +1,31 @@
 <script setup lang="ts">
-import { defineProps } from 'vue'
+import {computed, defineProps} from 'vue'
 import LootListItem from './LootListItem.vue'
 import {Loot} from "../utils/loot/loot.types.ts";
 import {groupLoot} from "../utils/loot/loot.helpers.ts";
+import LootListMenu from "./LootListMenu.vue";
+import * as _ from "lodash";
 
 const { loot } = defineProps<{ loot: Loot[] }>()
 const lootGrouped = groupLoot(loot)
 
+const totalLootValue = computed(() => {
+  const values = lootGrouped.map(lootEntry => lootEntry.item.value * lootEntry.amount)
+  return _.sum(values)
+})
+
 </script>
 
 <template>
+  <LootListMenu class="loot-list__menu" :total-loot-value="totalLootValue" />
   <LootListItem class="loot-list__list-item" v-for="lootEntry in lootGrouped" :key="lootEntry.item.name" :loot-entry="lootEntry"></LootListItem>
 </template>
 
 <style scoped>
+.loot-list__menu {
+  margin-bottom: 16px;
+}
+
 .loot-list__list-item {
   margin-bottom: 16px;
 }
---
+++ frontend/src/components/LootListMenu.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import { VCard, VCardText } from "vuetify/components/VCard";
+import { VBtn, VIcon, VChip } from 'vuetify/components'
+
+const { totalLootValue } = defineProps<{ totalLootValue: number }>()
+</script>
+
+<template>
+  <v-card class="loot-list-menu__card">
+    <v-card-text class="loot-list-menu__card-text">
+      <v-btn color="secondary">
+        Reset hunt
+      </v-btn>
+
+      <v-chip color="secondary">
+        {{ totalLootValue }}
+        <v-icon icon="mdi-gold"></v-icon>
+      </v-chip>
+    </v-card-text>
+  </v-card>
+</template>
+
+<style scoped>
+.loot-list-menu__card {
+  width: 100%;
+}
+
+.loot-list-menu__card-text {
+  display: flex;
+  justify-content: space-between;
+}
+</style>
