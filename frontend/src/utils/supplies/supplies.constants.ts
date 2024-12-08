a8821c9eeebab332d01b13d6653cc175b8c261c1
alex1431999 <alexhaller99@yahoo.de>
2024-12-08 16:01:44 +0100

    feat: add more supplies

--- frontend/src/utils/supplies/supplies.constants.ts
+++ frontend/src/utils/supplies/supplies.constants.ts
@@ -6,5 +6,49 @@ export const SUPPLIES = [
     {
         name: 'Explosion rune',
         value: 260,
-    }
+    },
+    {
+        name: 'Heavy magic missile rune',
+        value: 75,
+    },
+    {
+        name: 'Sudden death rune',
+        value: 300,
+    },
+    {
+        name: 'Great fireball rune',
+        value: 175,
+    },
+    {
+        name: 'Ring of healing',
+        value: 2000,
+    },
+    {
+        name: 'Backpacks of Mana fluids',
+        value: 2000,
+    },
+    {
+        name: 'Big mana Potion',
+        value: 850,
+    },
+    {
+        name: 'Ring of purity minutes',
+        value: 233.33,
+    },
+    {
+        name: 'Arrow',
+        value: 2,
+    },
+    {
+        name: 'Hunting arrow',
+        value: 4,
+    },
+    {
+        name: 'Bolt',
+        value: 3,
+    },
+    {
+        name: 'Hunting bolt',
+        value: 5    ,
+    },
 ]
