34a7e0323211f109a32df9880b06579a2e79fbe4
alex1431999 <alexhaller99@yahoo.de>
2024-12-09 21:31:02 +0100

    feat: add settings button

--- frontend/src/components/Header.vue
+++ frontend/src/components/Header.vue
@@ -1,10 +1,11 @@
 <script setup lang="ts">
-import { VToolbar, VToolbarTitle } from "vuetify/components";
+import { VToolbar, VToolbarTitle, VBtn } from "vuetify/components";
 
 </script>
 
 <template>
   <v-toolbar>
     <v-toolbar-title>Medivialyzer</v-toolbar-title>
+    <v-btn icon="mdi-cog" />
   </v-toolbar>
 </template>
