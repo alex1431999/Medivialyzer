64b4f46feeec69573eff4b2918c914cbca4fc225
alex1431999 <alexhaller99@yahoo.de>
2024-12-22 19:54:09 +0100

    feat: change color of profit indicator based on positive or negative

--- frontend/src/components/LootProfitDisplay.vue
+++ frontend/src/components/LootProfitDisplay.vue
@@ -1,11 +1,14 @@
 <script setup lang="ts">
 import {VChip, VIcon} from "vuetify/components";
+import {computed} from "vue";
 
 const { profit } = defineProps<{ profit: number }>()
+
+const color = computed(() => profit > 0 ? 'success' : 'error')
 </script>
 
 <template>
-  <v-chip color="success">
+  <v-chip :color="color">
     {{ profit }}
     <v-icon icon="mdi-gold"></v-icon>
   </v-chip>
