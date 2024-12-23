cadfe9041985f546e015f3b11202fb323cdbeaed
alex1431999 <alexhaller99@yahoo.de>
2024-12-23 19:46:33 +0100

    feat: add tooltips for time control

--- frontend/src/components/LootListTimeControl.vue
+++ frontend/src/components/LootListTimeControl.vue
@@ -1,12 +1,27 @@
 <script setup lang="ts">
-import { VBtn } from 'vuetify/components';
+import {VBtn, VChip, VIcon, VTooltip} from 'vuetify/components';
 </script>
 
 <template>
   <div class="button-container">
-    <v-btn @click="$emit('back')" size="small" color="secondary" icon="mdi-arrow-left" />
-    <v-btn @click="$emit('reset')" size="small" color="secondary" icon="mdi-refresh" />
-    <v-btn @click="$emit('forward')" size="small" color="secondary" icon="mdi-arrow-right" />
+    <v-tooltip location="top" text="Backwards 30 minutes">
+      <template v-slot:activator="{ props }">
+        <v-btn v-bind="props" @click="$emit('back')" size="small" color="secondary" icon="mdi-arrow-left" />
+      </template>
+    </v-tooltip>
+
+
+    <v-tooltip location="top" text="Reset hunt">
+      <template v-slot:activator="{ props }">
+        <v-btn v-bind="props" @click="$emit('reset')" size="small" color="secondary" icon="mdi-refresh" />
+      </template>
+    </v-tooltip>
+
+    <v-tooltip location="top" text="Forward 30 minutes">
+      <template v-slot:activator="{ props }">
+        <v-btn v-bind="props" @click="$emit('forward')" size="small" color="secondary" icon="mdi-arrow-right" />
+      </template>
+    </v-tooltip>
   </div>
 </template>
 
