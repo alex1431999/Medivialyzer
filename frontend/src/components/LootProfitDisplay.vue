680566af8b90d604c3259da42e4a2f6281b61233
alex1431999 <alexhaller99@yahoo.de>
2024-12-23 19:16:51 +0100

    feat: add tooltip to profit

--- frontend/src/components/LootProfitDisplay.vue
+++ frontend/src/components/LootProfitDisplay.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import {VChip, VIcon} from "vuetify/components";
+import {VChip, VIcon, VTooltip} from "vuetify/components";
 import {computed} from "vue";
 
 const { profit } = defineProps<{ profit: number }>()
@@ -8,8 +8,12 @@ const color = computed(() => profit > 0 ? 'success' : 'error')
 </script>
 
 <template>
-  <v-chip :color="color">
-    {{ profit }}
-    <v-icon icon="mdi-gold"></v-icon>
-  </v-chip>
+  <v-tooltip text="Profit">
+    <template v-slot:activator="{ props }">
+        <v-chip v-bind="props" :color="color">
+          {{ profit }}
+          <v-icon icon="mdi-gold"></v-icon>
+        </v-chip>
+      </template>
+  </v-tooltip>
 </template>
