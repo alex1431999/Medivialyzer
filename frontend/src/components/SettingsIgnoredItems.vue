c0bc1c925077987b8898baef7e89ccd367601935
alex1431999 <alexhaller99@yahoo.de>
2024-12-24 16:44:20 +0100

    feat: add empty state for ignored items

--- frontend/src/components/SettingsIgnoredItems.vue
+++ frontend/src/components/SettingsIgnoredItems.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 
-import {VBtn, VList, VListItem} from "vuetify/components";
+import {VBtn, VList, VListItem, VAlert} from "vuetify/components";
 import {computed} from "vue";
 import {useConfigStore} from "../stores/configStore.ts";
 
@@ -15,7 +15,7 @@ function onRemoveIgnoredItem(itemName: string) {
 </script>
 
 <template>
-  <v-list>
+  <v-list v-if="ignoredItems.length > 0">
     <v-list-item class="settings__list-item" v-for="itemName in ignoredItems" :key="itemName">
       <div class="settings__list-item__content">
         <div>{{ itemName }}</div>
@@ -23,6 +23,9 @@ function onRemoveIgnoredItem(itemName: string) {
       </div>
     </v-list-item>
   </v-list>
+  <div class="d-flex justify-center" v-else>
+    <v-alert text="You have not ignored an item yet" type="info" color="secondary"/>
+  </div>
 </template>
 
 <style scoped>
