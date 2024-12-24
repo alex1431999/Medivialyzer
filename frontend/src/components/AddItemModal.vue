5ac6e753648b23b32632e2f790abe5049abd1a73
alex1431999 <alexhaller99@yahoo.de>
2024-12-24 11:28:45 +0100

    feat: implement form

--- frontend/src/components/AddItemModal.vue
+++ frontend/src/components/AddItemModal.vue
@@ -1,10 +1,15 @@
 <script setup lang="ts">
-import { VDialog } from "vuetify/components";
+import { VDialog, VCard, VCardTitle, VCardText, VTextField, VForm, VCardActions, VBtn } from "vuetify/components";
 import {ref, watch} from "vue";
 
 const { itemToAddName } = defineProps<{ itemToAddName: string | null }>()
 
 const isOpen = ref<boolean>(false)
+const isValid = ref<boolean>(true)
+
+const valueRules = [
+    (value: string) => !!value || "Value is required",
+]
 
 watch(() => itemToAddName, () => {
   isOpen.value = itemToAddName !== null
@@ -13,9 +18,22 @@ watch(() => itemToAddName, () => {
 </script>
 
 <template>
-  <v-dialog v-model="isOpen">
-    <template v-slot:default>
-      {{ itemToAddName }}
+  <v-dialog max-width="600" v-model="isOpen">
+    <template #default>
+      <v-card>
+        <v-card-title>Add item</v-card-title>
+        <v-card-text>
+          <v-form v-model="isValid">
+            <v-text-field label="Name" required readonly :value="itemToAddName" variant="solo" />
+            <v-text-field label="Value" required placeholder="Value in gold" type="number" variant="solo" :rules="valueRules" />
+          </v-form>
+        </v-card-text>
+
+        <v-card-actions>
+          <v-btn :disabled="!isValid">Add</v-btn>
+          <v-btn @click="isOpen = false">Close</v-btn>
+        </v-card-actions>
+      </v-card>
     </template>
   </v-dialog>
 </template>
