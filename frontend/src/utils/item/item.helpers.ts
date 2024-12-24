7316633855176106adf8dddb770cc8a04f99e892
alex1431999 <alexhaller99@yahoo.de>
2024-12-24 11:44:11 +0100

    feat: add custom items to list of all items

--- frontend/src/utils/item/item.helpers.ts
+++ frontend/src/utils/item/item.helpers.ts
@@ -1,9 +1,18 @@
 import {Item, ItemName} from "./item.types.ts";
 import {ITEMS} from "./item.constants.ts";
 import {singularize} from "../string.ts";
+import {useConfigStore} from "../../stores/configStore.ts";
+
+export function getAllItems() {
+    const configStore = useConfigStore()
+
+    return [...ITEMS, ...configStore.config.customItems]
+}
 
 export function getItem(itemName: ItemName) {
-    return ITEMS.find((item: Item) => {
+    const allItems = getAllItems()
+    
+    return allItems.find((item: Item) => {
         const itemNameCurrent = item.name.toLowerCase()
         const itemNameLowerCase = itemName.toLowerCase()
         const itemNameSingular = singularize(itemNameLowerCase)
