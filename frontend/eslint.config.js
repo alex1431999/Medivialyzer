9cbf8bb15c4115c485c45b1ce2538497f18429f3
alex1431999 <alexhaller99@yahoo.de>
2024-12-27 20:35:23 +0100

    feat: add prettier

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,30 +1,32 @@
 // Import dependencies
-import vue from 'eslint-plugin-vue';
-import ts from '@typescript-eslint/eslint-plugin';
-import vueParser from 'vue-eslint-parser';
+import vue from "eslint-plugin-vue";
+import ts from "@typescript-eslint/eslint-plugin";
+import vueParser from "vue-eslint-parser";
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
 
 export default [
-    {
-        files: ['**/*.ts', '**/*.vue'],
-        ignores: ['node_modules/', 'dist/'],
-        languageOptions: {
-            parser: vueParser,
-            parserOptions: {
-                parser: '@typescript-eslint/parser',
-                ecmaVersion: 'latest',
-                sourceType: 'module',
-                ecmaFeatures: {
-                    jsx: true,
-                },
-            },
+  {
+    files: ["**/*.ts", "**/*.vue"],
+    ignores: ["node_modules/", "dist/"],
+    languageOptions: {
+      parser: vueParser,
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+        ecmaVersion: "latest",
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
         },
-        plugins: {
-            vue,
-            '@typescript-eslint': ts,
-        },
-        rules: {
-            'vue/no-unused-vars': 'error',
-            '@typescript-eslint/no-unused-vars': 'error',
-        },
+      },
     },
+    plugins: {
+      vue,
+      "@typescript-eslint": ts,
+    },
+    rules: {
+      "vue/no-unused-vars": "error",
+      "@typescript-eslint/no-unused-vars": "error",
+    },
+  },
+  eslintPluginPrettierRecommended,
 ];
