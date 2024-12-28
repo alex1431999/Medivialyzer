a383c310e5c166a77c0099fedd8ceff6d30d88e6
alex1431999 <alexhaller99@yahoo.de>
2024-12-28 17:26:53 +0100

    fix: warnings when running tests

--- frontend/test/setup.ts
+++ frontend/test/setup.ts
@@ -1,10 +1,10 @@
-import { beforeEach } from 'vitest'
+import { beforeAll } from 'vitest'
 import { createApp } from 'vue'
 import { createPinia, setActivePinia } from 'pinia'
 
 const app = createApp({})
 
-beforeEach(() => {
+beforeAll(() => {
   const pinia = createPinia()
   app.use(pinia)
   setActivePinia(pinia)
