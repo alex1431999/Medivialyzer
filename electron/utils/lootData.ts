1053342544b3a03364e521c313931006e458e960
alex1431999 <alexhaller99@yahoo.de>
2025-01-04 17:04:35 +0100

    feat: load loot data based on stored path

--- electron/utils/lootData.ts
+++ electron/utils/lootData.ts
@@ -1,12 +1,15 @@
 import fs from "node:fs";
 import path from "path";
 import * as os from "node:os";
+import { configStore } from '../stores/configStore';
 
 export const DEFAULT_LOOT_FILE_PATH = path.join(os.homedir(), 'medivia', 'Loot.txt');
 
 export function getLootData() {
+    const filePath = (configStore.get('config') as any).lootFilePath || DEFAULT_LOOT_FILE_PATH
+
     try {
-        return fs.readFileSync(DEFAULT_LOOT_FILE_PATH).toString()
+        return fs.readFileSync(filePath).toString()
     }
     catch (error) {
         return ''
