2bf4643fbda4e46953c81ad2a3320c800de95377
alex1431999 <alexhaller99@yahoo.de>
2025-01-04 18:43:36 +0100

    fix: allow manually updating the path

--- frontend/src/components/SettingsGeneral.vue
+++ frontend/src/components/SettingsGeneral.vue
@@ -1,28 +1,7 @@
 <script setup lang="ts">
-import { VTextField, VBtn } from 'vuetify/components'
-import { useConfigStore } from '../stores/configStore.ts'
-import { electron } from '../utils/electron/electron.constants.ts'
-
-const configStore = useConfigStore()
-
-async function selectLootFile() {
-  const path = await electron.openFileDialog()
-
-  if (path) {
-    configStore.setLootFilePath(path)
-  }
-}
+import SettingsLootFilePath from './SettingsLootFilePath.vue'
 </script>
 
 <template>
-  <div>
-    <div class="d-flex ga-2">
-      <v-text-field
-        persistent-placeholder
-        label="Loot file path"
-        :value="configStore.config.lootFilePath"
-      />
-      <v-btn @click="selectLootFile" icon="mdi-file-search" />
-    </div>
-  </div>
+  <SettingsLootFilePath />
 </template>
