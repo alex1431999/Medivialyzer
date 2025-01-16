d203e4ab45293723967faa63408652756bfb6b20
alex1431999 <alexhaller99@yahoo.de>
2025-01-16 07:54:54 +0100

    feat: add edit event

--- frontend/src/components/LootListItemMenu.vue
+++ frontend/src/components/LootListItemMenu.vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
 import { VList, VListItem, VMenu, VIcon } from 'vuetify/components'
 
-const emit = defineEmits(['ignore'])
+const emit = defineEmits(['ignore', 'edit'])
 </script>
 
 <template>
@@ -10,7 +10,8 @@ const emit = defineEmits(['ignore'])
       <v-icon v-bind="props" icon="mdi-dots-vertical"></v-icon>
     </template>
     <v-list>
-      <v-list-item @click="emit('ignore')"> Ignore item </v-list-item>
+      <v-list-item @click="emit('ignore')"> Ignore </v-list-item>
+      <v-list-item @click="emit('edit')"> Edit </v-list-item>
     </v-list>
   </v-menu>
 </template>
