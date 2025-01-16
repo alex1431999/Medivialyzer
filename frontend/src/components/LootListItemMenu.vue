a7795f7c25eac093419b2d43339105bb0790dd52
alex1431999 <alexhaller99@yahoo.de>
2025-01-16 07:53:33 +0100

    refactor: make loot list item menu a separate component

---
+++ frontend/src/components/LootListItemMenu.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { VList, VListItem, VMenu, VIcon } from 'vuetify/components'
+
+const emit = defineEmits(['ignore'])
+</script>
+
+<template>
+  <v-menu>
+    <template v-slot:activator="{ props }">
+      <v-icon v-bind="props" icon="mdi-dots-vertical"></v-icon>
+    </template>
+    <v-list>
+      <v-list-item @click="emit('ignore')"> Ignore item </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
