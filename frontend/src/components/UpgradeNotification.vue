9186521fd83b13f688e099781c34309ba73b6cd2
alex1431999 <alexhaller99@yahoo.de>
2025-01-26 21:38:05 +0100

    feat: implement close button

--- frontend/src/components/UpgradeNotification.vue
+++ frontend/src/components/UpgradeNotification.vue
@@ -1,13 +1,24 @@
 <script setup lang="ts">
 import { AppRemoteConfig, getConfig } from '../utils/make/make.requests.ts'
-import { VCard, VCardTitle, VCardText } from 'vuetify/components'
+import {
+  VCard,
+  VCardTitle,
+  VCardText,
+  VCardActions,
+  VBtn,
+} from 'vuetify/components'
 import { computed, onMounted, ref } from 'vue'
 import { APP_VERSION } from '../constants/app.ts'
 
 const config = ref<AppRemoteConfig | null>(null)
+const forceClose = ref<boolean>(false)
 
 const versionIsOutdated = computed(() => {
   return config.value && config.value.version !== APP_VERSION
+})
+
+const show = computed(() => {
+  return versionIsOutdated.value && !forceClose.value
 })
 
 const downloadLink = computed(() =>
@@ -20,9 +31,13 @@ onMounted(async () => {
 </script>
 
 <template>
-  <v-card v-if="versionIsOutdated" class="upgrade-notification" elevation="4">
+  <v-card v-if="show" class="upgrade-notification" elevation="4">
     <v-card-title> A new version is available! </v-card-title>
     <v-card-text>Download <a :href="downloadLink">here</a></v-card-text>
+    <v-card-actions>
+      <v-btn @click="forceClose = true">Close</v-btn>
+      <v-btn>Skip this version</v-btn>
+    </v-card-actions>
   </v-card>
 </template>
 
