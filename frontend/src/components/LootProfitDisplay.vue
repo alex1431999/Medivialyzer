f01bb227e07a7cc833557e32dbfd250b18e4bd1c
alex1431999 <alexhaller99@yahoo.de>
2025-01-30 20:37:41 +0100

    feat: format profit

--- frontend/src/components/LootProfitDisplay.vue
+++ frontend/src/components/LootProfitDisplay.vue
@@ -1,8 +1,11 @@
 <script setup lang="ts">
 import { VChip, VIcon, VTooltip } from 'vuetify/components'
 import { computed } from 'vue'
+import { formatNumber } from '../utils/number.ts'
 
 const { profit } = defineProps<{ profit: number }>()
+
+const profitFormatted = computed(() => formatNumber(profit))
 
 const color = computed(() => (profit > 0 ? 'success' : 'error'))
 </script>
@@ -11,7 +14,7 @@ const color = computed(() => (profit > 0 ? 'success' : 'error'))
   <v-tooltip text="Profit">
     <template v-slot:activator="{ props }">
       <v-chip v-bind="props" :color="color">
-        <span class="mr-1">{{ profit }}</span>
+        <span class="mr-1">{{ profitFormatted }}</span>
         <v-icon icon="mdi-gold"></v-icon>
       </v-chip>
     </template>
