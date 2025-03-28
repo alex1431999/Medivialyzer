1c3f96cc96fb7e93bcc427b1aab6bd3584f2baaf
alex1431999 <alexhaller99@yahoo.de>
2025-03-28 22:41:02 +0100

    feat: add average loot per kill to CreatureKilled.vue

--- frontend/src/components/CreatureKilled.vue
+++ frontend/src/components/CreatureKilled.vue
@@ -3,7 +3,14 @@ import {
   CreatureGrouped,
   CreatureWithAverageLoot,
 } from '../utils/creature/creature.types.ts'
-import { VChip, VBadge, VCard, VCardText } from 'vuetify/components'
+import {
+  VChip,
+  VBadge,
+  VCard,
+  VCardText,
+  VIcon,
+  VTooltip,
+} from 'vuetify/components'
 
 const { creature } = defineProps<{
   creature: CreatureGrouped
@@ -13,12 +20,23 @@ const { creature } = defineProps<{
 
 <template>
   <v-card>
-    <v-card-text>
+    <v-card-text class="d-flex justify-space-between">
       <v-badge color="secondary" :content="`${creature.amount}x`">
         <v-chip>
           {{ creature.name }}
         </v-chip>
       </v-badge>
+
+      <v-tooltip text="Average loot per kill">
+        <template v-slot:activator="{ props }">
+          <v-chip v-bind="props" color="secondary">
+            <span class="mr-1">{{
+              creatureWithAverageLoot.averageLootValue.toFixed(2)
+            }}</span>
+            <v-icon icon="mdi-gold"></v-icon>
+          </v-chip>
+        </template>
+      </v-tooltip>
     </v-card-text>
   </v-card>
 </template>
