396ba2716798e07742abae1cc54ef93815f11314
alex1431999 <alexhaller99@yahoo.de>
2025-05-23 13:04:11 +0200

    refactor: move getCreatureKillsPerHour to helper function

--- frontend/src/utils/creature/creature.helpers.ts
+++ frontend/src/utils/creature/creature.helpers.ts
@@ -1,8 +1,10 @@
 import {
   Creature,
+  CreatureEntry,
   CreatureGrouped,
   CreatureWithAverageLoot,
 } from './creature.types.ts'
+import _ from 'lodash'
 
 export function groupCreatures(creatures: Creature[]): CreatureGrouped[] {
   const creaturesMap: Record<Creature['name'], CreatureGrouped> = {}
@@ -33,4 +35,29 @@ export function getCreatureWithAverageLoot(
     )
 
   return creatureWithAverageLoot
+}
+
+export function getCreatureKillsPerHour(
+  creature: CreatureGrouped,
+  creatures: CreatureEntry[],
+): number | undefined {
+  const creaturesSorted = _.sortBy(creatures, 'timestamp')
+
+  const firstKill = creaturesSorted.find(
+    (creatureCurrent) => creatureCurrent.name === creature.name,
+  )
+
+  const lastKill = creaturesSorted
+    .reverse()
+    .find((creatureCurrent) => creatureCurrent.name === creature.name)
+
+  const start = firstKill?.timestamp || Date.now()
+  const end = lastKill?.timestamp || Date.now()
+
+  const kills = creature.amount
+
+  // We need at least 2 kills to get an understanding of how many kills an hour you get
+  if (start === end) return undefined
+
+  return Math.ceil((kills / (end - start)) * 1000 * 60 * 60)
 }
