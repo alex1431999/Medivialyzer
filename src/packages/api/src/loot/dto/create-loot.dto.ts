db2e675733df1c2753d66ad31e87775eb5c47857
alex1431999 <alexhaller99@yahoo.de>
2025-06-08 13:57:31 +0200

    feat: define dtos

--- src/packages/api/src/loot/dto/create-loot.dto.ts
+++ src/packages/api/src/loot/dto/create-loot.dto.ts
@@ -1,1 +1,19 @@
-export class CreateLootDto {}
+import { IsArray, IsDate, IsString, ValidateNested } from 'class-validator';
+import { ItemDto } from './item.dto';
+import { Type } from 'class-transformer';
+
+export class CreateLootDto {
+  @IsString()
+  clientId: string;
+
+  @IsString()
+  creatureName: string;
+
+  @IsDate()
+  timestamp: string;
+
+  @IsArray()
+  @ValidateNested({ each: true })
+  @Type(() => ItemDto)
+  items: ItemDto[];
+}
