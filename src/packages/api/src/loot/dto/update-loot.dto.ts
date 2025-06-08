76e2004c68ac0c33b19aaeaf4a6689c468757e78
alex1431999 <alexhaller99@yahoo.de>
2025-06-08 13:38:54 +0200

    deprecate: remove unused endpoints

--- src/packages/api/src/loot/dto/update-loot.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateLootDto } from './create-loot.dto';
-
-export class UpdateLootDto extends PartialType(CreateLootDto) {}
--- src/packages/api/src/loot/loot.controller.ts
+++ src/packages/api/src/loot/loot.controller.ts
@@ -9,7 +9,6 @@ import {
 } from '@nestjs/common';
 import { LootService } from './loot.service';
 import { CreateLootDto } from './dto/create-loot.dto';
-import { UpdateLootDto } from './dto/update-loot.dto';
 
 @Controller('loot')
 export class LootController {
@@ -18,25 +17,5 @@ export class LootController {
   @Post()
   create(@Body() createLootDto: CreateLootDto) {
     return this.lootService.create(createLootDto);
-  }
-
-  @Get()
-  findAll() {
-    return this.lootService.findAll();
-  }
-
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.lootService.findOne(+id);
-  }
-
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateLootDto: UpdateLootDto) {
-    return this.lootService.update(+id, updateLootDto);
-  }
-
-  @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.lootService.remove(+id);
   }
 }
