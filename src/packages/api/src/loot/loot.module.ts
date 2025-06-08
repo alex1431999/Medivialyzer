1dc5cd1bef1a8985470ff6a541f6453ee4bcb960
alex1431999 <alexhaller99@yahoo.de>
2025-06-08 14:51:59 +0200

    feat: add missing imports

--- src/packages/api/src/loot/loot.module.ts
+++ src/packages/api/src/loot/loot.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { LootService } from './loot.service';
 import { LootController } from './loot.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Loot } from './entities/loot.entity';
+import { Item } from './entities/item.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Loot, Item])],
   controllers: [LootController],
   providers: [LootService],
 })
