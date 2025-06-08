f93eeefc951098b19a36ff9a4df351cd2463813d
alex1431999 <alexhaller99@yahoo.de>
2025-06-08 14:01:50 +0200

    feat: define Loot entity

--- src/packages/api/src/loot/entities/loot.entity.ts
+++ src/packages/api/src/loot/entities/loot.entity.ts
@@ -1,1 +1,13 @@
-export class Loot {}
+import { Column, PrimaryGeneratedColumn } from 'typeorm';
+
+// TODO items are ignored for now
+export class Loot {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  creatureName: string;
+
+  @Column()
+  timestamp: number;
+}
