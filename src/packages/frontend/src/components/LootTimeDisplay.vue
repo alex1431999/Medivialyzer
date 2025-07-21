288fa93462f40b8df1143e1ef7bcefc973ec4324
alex1431999 <alexhaller99@yahoo.de>
2025-07-21 18:49:06 +0200

    feat: make the dates and time edible

--- src/packages/frontend/src/components/LootTimeDisplay.vue
+++ src/packages/frontend/src/components/LootTimeDisplay.vue
@@ -1,15 +1,38 @@
 <script setup lang="ts">
-import { computed } from 'vue'
-import { VChip } from 'vuetify/components'
+import { ref, watch } from 'vue'
+import { defineProps, defineEmits } from 'vue'
 import moment from 'moment'
+import { useConfigStore } from '../stores/configStore.ts'
 
-const { since } = defineProps<{ since: number }>()
+const props = defineProps<{ since: number }>()
+const emit = defineEmits<{
+  (e: 'update:since', value: number): void
+}>()
 
-const sinceFormatted = computed(() => {
-  return moment(since).format('DD. MMM HH:mm')
+const configStore = useConfigStore()
+
+// Initialize date and time strings from props.since
+const dateString = ref(moment(props.since).format('YYYY-MM-DD'))
+const timeString = ref(moment(props.since).format('HH:mm'))
+
+watch([dateString, timeString], ([d, t]) => {
+  if (!d || !t) return
+  const combined = moment(`${d}T${t}`)
+
+  if (combined.isValid()) {
+    configStore.setConfig({ since: combined.valueOf() })
+  }
 })
 </script>
 
 <template>
-  <v-chip> Loot since: {{ sinceFormatted }} </v-chip>
+  <div class="d-flex gap-2 align-center">
+    <input type="date" class="v-input__control" v-model="dateString" />
+    <input
+      type="time"
+      class="v-input__control"
+      v-model="timeString"
+      step="60"
+    />
+  </div>
 </template>
