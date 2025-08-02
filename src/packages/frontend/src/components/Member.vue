e2c7c5b6ef42fb0db072e39a0b4a6d8d7e684285
alex1431999 <alexhaller99@yahoo.de>
2025-08-02 17:33:02 +0200

    feat: add members section

---
+++ src/packages/frontend/src/components/Member.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+import { VCard, VCardText } from 'vuetify/components'
+</script>
+
+<template>
+  <v-card>
+    <v-card-text>Member</v-card-text>
+  </v-card>
+</template>
--- src/packages/frontend/src/components/TeamModal.vue
+++ src/packages/frontend/src/components/TeamModal.vue
@@ -7,6 +7,7 @@ import {
   VDialog,
   VTextField,
 } from 'vuetify/components'
+import Members from './Members.vue'
 </script>
 
 <template>
@@ -26,7 +27,8 @@ import {
           <div>Team</div>
         </v-card-title>
         <v-card-text>
-          <v-text-field label="Display name" placeholder="Anonymous" />
+          <v-text-field label="Your display name" placeholder="Anonymous" />
+          <Members />
         </v-card-text>
       </v-card>
     </template>
