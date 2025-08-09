a46601c29d5120f66823a1f616831556598862ff
alex1431999 <alexhaller99@yahoo.de>
2025-08-09 21:46:12 +0200

    test: that a team can be created

--- src/packages/api/src/team/team.service.spec.ts
+++ src/packages/api/src/team/team.service.spec.ts
@@ -1,20 +1,28 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { TeamService } from './team.service';
 import { TEST_DB_IMPORTS } from '../../test/test-utils';
+import { ClientService } from '../client/client.service';
+import { Client } from 'src/client/entities/client.entity';
 
 describe('TeamService', () => {
-  let service: TeamService;
+  let teamService: TeamService;
+  let clientService: ClientService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [TeamService],
+      providers: [TeamService, ClientService],
       imports: TEST_DB_IMPORTS,
     }).compile();
 
-    service = module.get<TeamService>(TeamService);
+    teamService = module.get<TeamService>(TeamService);
+    clientService = module.get<ClientService>(ClientService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  it('can create team', async () => {
+    await clientService.create({ id: 123 });
+    await teamService.create({
+      name: 'test',
+      owner: 123,
+    });
   });
 });
