5f55bcc487fdd200b99a30f1209f2a48c106c50e
alex1431999 <alexhaller99@yahoo.de>
2025-08-10 18:21:43 +0200

    fix: add missing decorators

--- src/packages/api/src/team/dto/update-team.dto.ts
+++ src/packages/api/src/team/dto/update-team.dto.ts
@@ -1,7 +1,10 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateTeamDto } from './create-team.dto';
+import { ApiProperty } from '@nestjs/swagger';
 
 export class UpdateTeamDto extends PartialType(CreateTeamDto) {
+  @ApiProperty()
   name?: string;
+
   owner?: never;
 }
