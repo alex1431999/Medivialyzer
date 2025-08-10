72fe56a68661499e85158f7658898b8f31975cf0
alex1431999 <alexhaller99@yahoo.de>
2025-08-10 20:37:01 +0200

    refactor: make base path a constant

--- src/packages/frontend/src/utils/api/api.client.ts
+++ src/packages/frontend/src/utils/api/api.client.ts
@@ -1,3 +1,4 @@
 import { ClientApi } from '../generated/api-client'
+import { API_BASE_PATH } from './api.constants.ts'
 
-export const clientApi = new ClientApi(undefined, 'http://localhost:3001')
+export const clientApi = new ClientApi(undefined, API_BASE_PATH)
--- src/packages/frontend/src/utils/api/api.team.ts
+++ src/packages/frontend/src/utils/api/api.team.ts
@@ -1,3 +1,4 @@
 import { TeamApi } from '../generated/api-client'
+import { API_BASE_PATH } from './api.constants.ts'
 
-export const teamApi = new TeamApi()
+export const teamApi = new TeamApi(undefined, API_BASE_PATH)
