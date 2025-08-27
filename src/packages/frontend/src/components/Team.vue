ed7f88101cfe46d6b37bedc706b7dbd15a13c8ce
alex1431999 <alexhaller99@yahoo.de>
2025-08-27 20:20:20 +0200

    feat: add edit button for the team name

--- src/packages/frontend/src/components/Team.vue
+++ src/packages/frontend/src/components/Team.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 import Members from './Members.vue'
 import { Team } from '../stores/teamStore.ts'
+import { VTextField, VBtn } from 'vuetify/components'
 
 const { team } = defineProps<{ team: Team }>()
 </script>
 
 <template>
-  <div class="d-flex flex-column">
-    <h2>{{ team.name }}</h2>
+  <div class="d-flex flex-column w-100">
+    <div class="d-flex w-100">
+      <v-text-field
+        readonly
+        variant="solo"
+        :value="team.name"
+        class="mr-2"
+      ></v-text-field>
+      <v-btn icon="mdi-pen"></v-btn>
+    </div>
     <Members :members="team.members || []" />
   </div>
 </template>
