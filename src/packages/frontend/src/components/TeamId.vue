dcc3cb4b0294ee7144add948756591fb13300696
alex1431999 <alexhaller99@yahoo.de>
2025-09-06 21:20:21 +0200

    feat: add TeamId to team overview

---
+++ src/packages/frontend/src/components/TeamId.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import { VBtn, VIcon, VTextField } from 'vuetify/components'
+
+const props = defineProps<{ id: string }>()
+
+const handleCopy = async () => {
+  await navigator.clipboard.writeText(props.id)
+}
+</script>
+
+<template>
+  <div class="d-flex ga-2">
+    <v-text-field
+      disabled
+      hide-details
+      label="Team ID"
+      :model-value="props.id"
+    />
+
+    <v-btn icon variant="text" color="primary" @click="handleCopy">
+      <v-icon color="secondary"> mdi-content-copy </v-icon>
+    </v-btn>
+  </div>
+</template>
---
+++ src/packages/frontend/src/components/TeamName.vue
@@ -0,0 +1,36 @@
+<script setup lang="ts">
+import { VBtn, VTextField } from 'vuetify/components'
+import { Team } from '../stores/teamStore.ts'
+import { ref, watch } from 'vue'
+
+const { team } = defineProps<{ team: Team }>()
+const emit = defineEmits(['updateName'])
+const editNameMode = ref<boolean>(false)
+
+const nameLocal = ref<string>(team.name)
+
+watch(
+  () => team.name,
+  (name) => (nameLocal.value = name),
+)
+
+function onEditNameSave() {
+  editNameMode.value = false
+  emit('updateName', nameLocal.value)
+}
+</script>
+
+<template>
+  <div class="d-flex ga-2">
+    <v-text-field
+      v-model="nameLocal"
+      variant="solo-filled"
+      label="Name"
+      hide-details
+      :readonly="!editNameMode"
+      :disabled="!editNameMode"
+    ></v-text-field>
+    <v-btn v-if="editNameMode" icon="mdi-check" @click="onEditNameSave"></v-btn>
+    <v-btn v-else icon="mdi-pencil" @click="editNameMode = true"></v-btn>
+  </div>
+</template>
