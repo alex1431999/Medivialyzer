032d574a241776522d6c6a50bb5b429dfa231809
alex1431999 <alexhaller99@yahoo.de>
2025-09-06 21:46:20 +0200

    feat: add isYou badge to members

--- src/packages/frontend/src/components/Member.vue
+++ src/packages/frontend/src/components/Member.vue
@@ -1,13 +1,25 @@
 <script setup lang="ts">
-import { VCard, VCardText } from 'vuetify/components'
+import { VCard, VCardText, VBadge, VChip } from 'vuetify/components'
 import { Member } from '../stores/teamStore.ts'
-import { Client } from '../stores/clientStore.ts'
+import { Client, useClientStore } from '../stores/clientStore.ts'
+import { computed } from 'vue'
+
+const clientStore = useClientStore()
 
 const { member } = defineProps<{ member: Member | Client }>()
+
+const isYou = computed(() => clientStore.client?.id === member.id)
 </script>
 
 <template>
   <v-card>
-    <v-card-text>{{ member.name }}</v-card-text>
+    <v-card-text>
+      <v-badge v-if="!isYou"> {{ member.name }} </v-badge>
+      <v-badge v-else color="secondary" content="You">
+        <v-chip>
+          {{ member.name }}
+        </v-chip>
+      </v-badge>
+    </v-card-text>
   </v-card>
 </template>
