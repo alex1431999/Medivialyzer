7ea5d22b7983e42255097554bb58fb3df7bdff30
alex1431999 <alexhaller99@yahoo.de>
2025-09-16 20:59:21 +0200

    feat: add empty state for waste

--- src/packages/frontend/src/components/Waste.vue
+++ src/packages/frontend/src/components/Waste.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { VChip } from 'vuetify/components'
+import { VChip, VIcon } from 'vuetify/components'
 import { Waste } from '../stores/teamStore.ts'
 
 const { waste } = defineProps<{ waste?: Waste }>()
@@ -8,5 +8,8 @@ const { waste } = defineProps<{ waste?: Waste }>()
 <template>
   <v-chip v-if="waste !== undefined" color="warning" append-icon="mdi-gold">
     {{ waste.wasteAmount }}
+  </v-chip>
+  <v-chip v-else class="opacity-20" color="warning">
+    <v-icon>mdi-gold</v-icon>
   </v-chip>
 </template>
