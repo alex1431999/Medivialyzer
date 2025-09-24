85a9804d19990588582d746d3cd73380251b3ac1
alex1431999 <alexhaller99@yahoo.de>
2025-09-24 21:14:10 +0200

    fix: failing tests

--- src/packages/api/src/team/team.controller.spec.ts
+++ src/packages/api/src/team/team.controller.spec.ts
@@ -2,6 +2,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 import { TeamController } from './team.controller';
 import { TeamService } from './team.service';
 import { TEST_DB_IMPORTS } from '../../test/test-utils';
+import { TeamGateway } from './team.gateway';
+import { ClientService } from '../client/client.service';
 
 describe('TeamController', () => {
   let controller: TeamController;
@@ -9,7 +11,7 @@ describe('TeamController', () => {
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [TeamController],
-      providers: [TeamService],
+      providers: [TeamService, TeamGateway, ClientService],
       imports: TEST_DB_IMPORTS,
     }).compile();
 
