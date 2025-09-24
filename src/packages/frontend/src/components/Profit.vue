4b1221abce2bcc0595abc2cea0f9ba8394be40aa
alex1431999 <alexhaller99@yahoo.de>
2025-09-24 21:19:32 +0200

    feat: add tooltip to waste and profit

--- src/packages/frontend/src/components/Profit.vue
+++ src/packages/frontend/src/components/Profit.vue
@@ -1,18 +1,23 @@
 <script setup lang="ts">
-import { VChip, VIcon } from 'vuetify/components'
+import { VChip, VIcon, VTooltip } from 'vuetify/components'
 
 const { profitAmount } = defineProps<{ profitAmount?: number }>()
 </script>
 
 <template>
-  <v-chip
-    v-if="profitAmount !== undefined"
-    color="secondary"
-    append-icon="mdi-gold"
-  >
-    {{ profitAmount }}
-  </v-chip>
-  <v-chip v-else class="opacity-20" color="success">
-    <v-icon>mdi-gold</v-icon>
-  </v-chip>
+  <v-tooltip text="Payout" location="top">
+    <template v-slot:activator="{ props }">
+      <v-chip
+        v-if="profitAmount !== undefined"
+        v-bind="props"
+        color="secondary"
+        append-icon="mdi-gold"
+      >
+        {{ profitAmount }}
+      </v-chip>
+      <v-chip v-else v-bind="props" class="opacity-20" color="success">
+        <v-icon>mdi-gold</v-icon>
+      </v-chip>
+    </template>
+  </v-tooltip>
 </template>
--- src/packages/frontend/src/components/Waste.vue
+++ src/packages/frontend/src/components/Waste.vue
@@ -1,15 +1,24 @@
 <script setup lang="ts">
-import { VChip, VIcon } from 'vuetify/components'
+import { VChip, VIcon, VTooltip } from 'vuetify/components'
 import { WasteDto } from '../utils/generated/api-client'
 
 const { waste } = defineProps<{ waste?: WasteDto }>()
 </script>
 
 <template>
-  <v-chip v-if="waste !== undefined" color="warning" append-icon="mdi-gold">
-    {{ waste.wasteAmount }}
-  </v-chip>
-  <v-chip v-else class="opacity-20" color="warning">
-    <v-icon>mdi-gold</v-icon>
-  </v-chip>
+  <v-tooltip text="Waste" location="top">
+    <template v-slot:activator="{ props }">
+      <v-chip
+        v-bind="props"
+        v-if="waste !== undefined"
+        color="warning"
+        append-icon="mdi-gold"
+      >
+        {{ waste.wasteAmount }}
+      </v-chip>
+      <v-chip v-bind="props" v-else class="opacity-20" color="warning">
+        <v-icon>mdi-gold</v-icon>
+      </v-chip>
+    </template>
+  </v-tooltip>
 </template>
