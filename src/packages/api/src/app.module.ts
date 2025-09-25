6c52413f0cb7ea4988d850be657d66e19dec503f
alex1431999 <alexhaller99@yahoo.de>
2025-09-25 19:00:45 +0200

    fix: default value for database name

--- src/packages/api/src/app.module.ts
+++ src/packages/api/src/app.module.ts
@@ -5,7 +5,7 @@ import { ClientModule } from './client/client.module';
 
 const username = process.env.PGUSER || 'postgres';
 const password = process.env.PGPASSWORD || 'postgres';
-const database = process.env.PGDATABASE || 'postgres';
+const database = process.env.PGDATABASE || 'medivialyzer';
 const host = process.env.PGHOST || 'database';
 const port = parseInt(process.env.PGPORT || '5432', 10);
 
