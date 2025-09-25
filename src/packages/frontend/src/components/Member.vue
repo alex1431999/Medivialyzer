d43760817d91748d9a016dfc5429b09a8e27de79
alex1431999 <alexhaller99@yahoo.de>
2025-09-25 12:31:37 +0200

    feat: implement member deletion

--- src/packages/frontend/src/components/Member.vue
+++ src/packages/frontend/src/components/Member.vue
@@ -1,6 +1,6 @@
 <script setup lang="ts">
 import { VCard, VCardText, VBadge, VChip } from 'vuetify/components'
-import { Member, Team } from '../stores/teamStore.ts'
+import { Member, Team, useTeamStore } from '../stores/teamStore.ts'
 import { Client, useClientStore } from '../stores/clientStore.ts'
 import Waste from './Waste.vue'
 import { computed } from 'vue'
@@ -17,10 +17,14 @@ import {
   calculateMemberPayout,
   getMemberWaste,
 } from '../utils/waste/waste.utils.ts'
+import MemberMenu from './MemberMenu.vue'
+import { useNotifications } from '../composables/useNotifications.ts'
 
 const clientStore = useClientStore()
 const configStore = useConfigStore()
 const lootDataStore = useLootDataStore()
+const teamStore = useTeamStore()
+const { notify } = useNotifications()
 
 const { member, team } = defineProps<{
   member: Member | Client
@@ -39,10 +43,19 @@ const loot = computed(() => {
 const totalLootValue = computed(() => calcualteTotalLootValue(loot.value))
 
 const isYou = computed(() => clientStore.client?.id === member.id)
+const isClientOwner = computed(() => clientStore.client?.id === team.owner.id)
 const waste = computed(() => getMemberWaste(member.id, team.wastes))
 const payout = computed(() =>
   calculateMemberPayout(member.id, team.wastes, totalLootValue.value),
 )
+const canRemoveMember = computed(() => isClientOwner.value && !isYou.value)
+
+async function removeMember() {
+  if (canRemoveMember.value) {
+    await teamStore.removeMember(team.id, member.id)
+    notify('Member has been removed', 'success')
+  }
+}
 </script>
 
 <template>
@@ -56,9 +69,10 @@ const payout = computed(() =>
           </v-chip>
         </v-badge>
 
-        <div class="d-flex ga-2">
+        <div class="d-flex ga-2 align-center">
           <Waste :waste="waste" />
           <Payout :payout="payout"></Payout>
+          <MemberMenu :can-remove="canRemoveMember" @remove="removeMember" />
         </div>
       </div>
     </v-card-text>
---
+++ src/packages/frontend/src/components/MemberMenu.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import { VList, VListItem, VMenu, VIcon } from 'vuetify/components'
+
+const { canRemove } = defineProps<{ canRemove: boolean }>()
+
+const emit = defineEmits(['remove'])
+</script>
+
+<template>
+  <v-menu>
+    <template v-slot:activator="{ props }">
+      <v-icon v-bind="props" icon="mdi-dots-vertical"></v-icon>
+    </template>
+    <v-list>
+      <v-list-item :disabled="!canRemove" @click="emit('remove')">
+        Remove
+      </v-list-item>
+    </v-list>
+  </v-menu>
+</template>
