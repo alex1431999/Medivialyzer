8a058151dd9b82feb79cec05c0453a32f025819e
alex1431999 <alexhaller99@yahoo.de>
2025-10-01 20:46:45 +0200

    test: that a team can be created

--- src/packages/api/test/team.e2e-spec.ts
+++ src/packages/api/test/team.e2e-spec.ts
@@ -1,17 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication, ValidationPipe } from '@nestjs/common';
 import * as request from 'supertest';
-import { AppModule } from '../src/app.module';
-import { App } from 'supertest/types';
 import { TEST_DB_IMPORTS } from './test-utils';
-import { TeamModule } from '../src/team/team.module';
+import { TeamController } from '../src/team/team.controller';
+import { ClientController } from '../src/client/client.controller';
+import { TeamService } from '../src/team/team.service';
+import { TeamGateway } from '../src/team/team.gateway';
+import { ClientService } from '../src/client/client.service';
+import { App } from 'supertest/types';
 
 describe('TeamController (e2e)', () => {
   let app: INestApplication<App>;
 
   beforeEach(async () => {
     const moduleFixture: TestingModule = await Test.createTestingModule({
-      imports: [TeamModule, ...TEST_DB_IMPORTS],
+      imports: TEST_DB_IMPORTS,
+      controllers: [TeamController, ClientController],
+      providers: [TeamService, TeamGateway, ClientService],
     }).compile();
 
     app = moduleFixture.createNestApplication();
@@ -21,6 +26,18 @@ describe('TeamController (e2e)', () => {
 
   afterEach(async () => {
     await app.close();
+  });
+
+  it('/team (POST) is able to create a team', async () => {
+    // First create an owner client
+    await request(app.getHttpServer())
+      .post('/client')
+      .send({ id: 'owner-e2e', name: 'owner-e2e' });
+
+    return request(app.getHttpServer())
+      .post('/team')
+      .send({ owner: 'owner-e2e', name: 'team' })
+      .expect(201);
   });
 
   it('/team (POST) should not accept empty value for name', async () => {
