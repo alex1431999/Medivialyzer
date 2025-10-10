e4ffb5367379d31454076d5c57283b9b44b2beec
alex1431999 <alexhaller99@yahoo.de>
2025-10-10 17:25:00 +0200

    fix: rename profit to "solo hunt profit"

--- src/packages/frontend/src/components/loot/LootProfitDisplay.vue
+++ src/packages/frontend/src/components/loot/LootProfitDisplay.vue
@@ -11,7 +11,7 @@ const color = computed(() => (profit >= 0 ? 'success' : 'error'))
 </script>
 
 <template>
-  <v-tooltip text="Profit">
+  <v-tooltip text="Solo hunt profit">
     <template v-slot:activator="{ props }">
       <v-chip v-bind="props" :color="color">
         <span class="mr-1">{{ profitFormatted }}</span>
