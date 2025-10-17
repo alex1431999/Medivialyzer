18d065e865c6e495dea98df1f52cc4e52d0f5eb0
alex1431999 <alexhaller99@yahoo.de>
2025-10-17 17:47:42 +0200

    feat: add color to the supplies text fields

--- src/packages/frontend/src/components/supplies/SuppliesTable.vue
+++ src/packages/frontend/src/components/supplies/SuppliesTable.vue
@@ -40,6 +40,19 @@ const headers = [
 function onFavoriteClick(item: SuppliesTableItem, value: boolean) {
   emit('update:favorite', { item, value })
 }
+
+function itemHasBeforeValue(item: SuppliesTableItem) {
+  const before = suppliesStore.supplies[item.name].before
+  return before !== undefined && before !== null && before !== ''
+}
+
+function getBeforeColor(item: SuppliesTableItem): string | undefined {
+  return itemHasBeforeValue(item) ? 'success' : undefined
+}
+
+function getAfterColor(item: SuppliesTableItem): string | undefined {
+  return itemHasBeforeValue(item) ? 'warning' : undefined
+}
 </script>
 
 <template>
@@ -113,6 +126,8 @@ function onFavoriteClick(item: SuppliesTableItem, value: boolean) {
         density="compact"
         hide-details
         class="pa-2"
+        :color="getBeforeColor(item)"
+        :base-color="getBeforeColor(item)"
       />
     </template>
 
@@ -123,6 +138,8 @@ function onFavoriteClick(item: SuppliesTableItem, value: boolean) {
         variant="outlined"
         density="compact"
         hide-details
+        :color="getAfterColor(item)"
+        :base-color="getAfterColor(item)"
       />
     </template>
 
