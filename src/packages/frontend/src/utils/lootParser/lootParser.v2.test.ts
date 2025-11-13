03507a43296072ef7ced4cf13fbba9d644d59660
alex1431999 <alexhaller99@yahoo.de>
2025-11-13 20:16:35 +0100

    feat: implement basic version

---
+++ src/packages/frontend/src/utils/lootParser/lootParser.v2.test.ts
@@ -0,0 +1,186 @@
+import { describe, expect, test } from 'vitest'
+import { LootParserV2 as LootParser } from './lootParser.v2.ts'
+
+describe('lootParser', () => {
+  describe('loot', () => {
+    test('no loot', () => {
+      const lootParser = new LootParser()
+      expect(lootParser.parse('')).toEqual({})
+    })
+
+    test('items', () => {
+      const lootData = `
+        Channel saved at Wed Dec 04 19:29:20 2024
+        19:14 Loot of giant cobra: a gold coin.
+        `
+      const lootParser = new LootParser()
+      expect(lootParser.parse(lootData)).toEqual({
+        ['giant cobra']: {
+          items: [
+            {
+              amount: 1,
+              name: 'gold coin',
+            },
+          ],
+          count: 1,
+          creature: {
+            name: 'giant cobra',
+          },
+        },
+      })
+    })
+
+    test('plural', () => {
+      const lootData = `
+        Channel saved at Wed Dec 04 19:29:20 2024
+        19:14 Loot of giant cobra: 66 gold coins.
+        `
+
+      const lootParser = new LootParser()
+      expect(lootParser.parse(lootData)).toEqual({
+        ['giant cobra']: {
+          items: [
+            {
+              amount: 66,
+              name: 'gold coin',
+            },
+          ],
+          count: 1,
+          creature: {
+            name: 'giant cobra',
+          },
+        },
+      })
+    })
+
+    /**
+     * Sometimes medivia writes the loot as multiple lines in to the loot.txt.
+     * We deal with that bug by trying to piece together the line
+     */
+    test('multiline bug', () => {
+      const lootData = `
+        Channel saved at Wed Dec 04 19:29:20 2024
+        19:14 Loot of 
+        giant 
+        cobra: a gold 
+        coin.
+        `
+      const lootParser = new LootParser()
+      expect(lootParser.parse(lootData)).toEqual({
+        ['giant cobra']: {
+          items: [
+            {
+              amount: 1,
+              name: 'gold coin',
+            },
+          ],
+          count: 1,
+          creature: {
+            name: 'giant cobra',
+          },
+        },
+      })
+    })
+
+    test('multiline bug (complex)', () => {
+      const lootData = `
+        Channel saved at Wed Dec 04 19:29:20 2024
+        19:14 Loot of 
+        giant 
+        cobra: a gold 
+        coin.
+        
+        Channel saved at Wed Dec 04 19:40:20 2025
+        19:12 Loot of giant 
+        cobra: 12 gold coins.
+        `
+      const lootParser = new LootParser()
+      expect(lootParser.parse(lootData)).toEqual({
+        ['giant cobra']: {
+          items: [
+            {
+              amount: 1,
+              name: 'gold coin',
+            },
+            {
+              amount: 12,
+              name: 'gold coin',
+            },
+          ],
+          count: 2,
+          creature: {
+            name: 'giant cobra',
+          },
+        },
+      })
+    })
+  })
+
+  describe('creatures', () => {
+    test('no creatures', () => {
+      const lootData = `
+        Channel saved at Wed Dec 04 19:29:20 2024
+        `
+      const lootParser = new LootParser()
+
+      expect(lootParser.parse(lootData)).toEqual({})
+    })
+
+    test('one creature', () => {
+      const lootData = `
+        Channel saved at Wed Dec 04 19:29:20 2024
+        19:19 Loot of giant cobra: 9 gold coins.
+        `
+      const lootParser = new LootParser()
+      expect(lootParser.parse(lootData)).toEqual({
+        ['giant cobra']: {
+          items: [
+            {
+              amount: 9,
+              name: 'gold coin',
+            },
+          ],
+          count: 1,
+          creature: {
+            name: 'giant cobra',
+          },
+        },
+      })
+    })
+
+    test('many creatures', () => {
+      const lootData = `
+        Channel saved at Wed Dec 04 19:29:20 2024
+        19:19 Loot of giant cobra: 9 gold coins.
+        19:19 Loot of winged vermin: 17 gold coins.
+        `
+      const lootParser = new LootParser()
+      expect(lootParser.parse(lootData)).toEqual({
+        ['giant cobra']: {
+          items: [
+            {
+              amount: 9,
+              name: 'gold coin',
+            },
+          ],
+          count: 1,
+          creature: {
+            name: 'giant cobra',
+          },
+        },
+        ['winged vermin']: {
+          items: [
+            {
+              amount: 17,
+              name: 'gold coin',
+            },
+          ],
+          count: 1,
+          creature: {
+            name: 'winged vermin',
+          },
+        },
+      })
+    })
+  })
+})
---
+++ src/packages/frontend/src/utils/lootParser/lootParser.v2.ts
@@ -0,0 +1,103 @@
+import { Creature } from '../creature/creature.types.ts'
+import { ItemLooted } from '../item/item.types.ts'
+import { lootDataTypeItems } from './lootDataType/lootDataTypes/lootDataType.items.ts'
+import { lootDataTypeCreature } from './lootDataType/lootDataTypes/lootdataType.creature.ts'
+
+type CreaturesToLootMapEntry = {
+  creature: Creature
+  items: ItemLooted[]
+  count: number
+}
+
+type CreaturesToLootMap = Record<string, CreaturesToLootMapEntry>
+
+export class LootParserV2 {
+  public parse(lootData: string): CreaturesToLootMap {
+    let creaturesToLootMap: CreaturesToLootMap = {}
+
+    this.forEachLine(lootData, (line) => {
+      // Creatures with average loot
+      if (
+        lootDataTypeCreature.matches(line) ||
+        lootDataTypeCreature.matchesBag(line)
+      ) {
+        creaturesToLootMap = this.handleAverageLoot(line, creaturesToLootMap)
+      }
+    })
+
+    return creaturesToLootMap
+  }
+
+  private handleAverageLoot(
+    line: string,
+    creaturesToLootMap: CreaturesToLootMap,
+  ): CreaturesToLootMap {
+    const itemsLooted = lootDataTypeItems.toValue(line, [])
+    const isBagLoot = lootDataTypeCreature.matchesBag(line)
+
+    const creature = isBagLoot
+      ? lootDataTypeCreature.toValueForBag(line)
+      : lootDataTypeCreature.toValue(line)
+
+    const creatureAlreadyExists = Object.keys(creaturesToLootMap).includes(
+      creature.name,
+    )
+
+    if (!creatureAlreadyExists) {
+      creaturesToLootMap[creature.name] = {
+        creature,
+        items: itemsLooted,
+        count: 1,
+      }
+    } else {
+      const { items, count } = creaturesToLootMap[creature.name]
+
+      // If the loot came in a bag, then we don't count the extra kill
+      const countUpdate = isBagLoot ? count : count + 1
+
+      creaturesToLootMap[creature.name] = {
+        creature,
+        items: [...items, ...itemsLooted],
+        count: countUpdate,
+      }
+    }
+
+    return creaturesToLootMap
+  }
+
+  private forEachLine(lootData: string, callback: (line: string) => void) {
+    const lines = lootData
+      .split('\n') // Split by break lines
+      .map((line) => line.trim()) // Remove any extra white space
+      .filter((line) => !!line) // Get rid of all the empty lines
+
+    let linesPiecedTogehter = []
+    let fullLine = ''
+
+    for (let i = 0; i < lines.length; ++i) {
+      const currentLine = lines[i]
+      const nextLine = lines[i + 1]
+
+      // We add a space here because when lines go over multiple liens the space gets lost
+      fullLine += `${currentLine} `
+
+      // If there is no next line or the next line starts a new line we completed the current line
+      if (nextLine === undefined || this.isValidStartOfLine(nextLine)) {
+        linesPiecedTogehter.push(fullLine.trim())
+        fullLine = ''
+      }
+    }
+
+    linesPiecedTogehter.forEach(callback)
+  }
+
+  private isValidStartOfLine(line: string) {
+    const VALID_OPTIONS = [
+      /^channel saved at/, // String starts with "channel saved at"
+      /^\d{2}:\d{2}/, // String starts with HH:MM
+      /^$/, // Empty string
+    ]
+
+    return VALID_OPTIONS.some((pattern) => pattern.test(line.toLowerCase()))
+  }
+}
